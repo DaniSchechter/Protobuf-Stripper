0913dd81b8b82a7a7de9e3af57f373e70b600003
alon blecher <alonbl12@gmail.com>
2019-05-26 18:12:00 +0300

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,40 @@
-#include "PortsTest.hpp"
-#include "IpBlackList.hpp"
-#include "IsEncryptedRawData.hpp"
-#include <iostream>
-void main() {
-	if (port_density("1.1.1.1", "2.2.2.2", "45", "4000"))//no
-		std::cout << "yes\n";
-	else
-		std::cout << "no\n";
-	if (port_density("2.1.1.1", "2.2.2.2", "45", "4000"))//yes
-		std::cout << "yes\n";
-	else
-		std::cout << "no\n";
-	if (port_density("3.1.1.1", "2.2.2.2", "45", "4000"))//yes
-		std::cout << "yes\n";
-	else
-		std::cout << "no\n";
-	if (port_density("4.1.1.1", "2.2.2.2", "45", "4000"))//yes
-		std::cout << "yes\n";
-	else
-		std::cout << "no\n";
-	if (port_density("4.1.1.1", "2.2.2.2", "45", "11"))//no
-		std::cout << "yes\n";
-	else
-		std::cout << "no\n";
+#include "server.hpp"
+#include "config.hpp"
+#include "logger.hpp"
 
-	///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	std::cout << "////////////////////////////////////////////////////////\n";
-	if (request_density("1.1.1.1", "205.209.143.94", "1"))//yes
-		std::cout << "yes\n";
-	else
-		std::cout << "no\n";
-	if (request_density("2.1.1.1", "2.2.2.2","1"))//no
-		std::cout << "yes\n";
-	else
-		std::cout << "no\n";
-	if (request_density("3.1.1.1", "2.2.2.2", "1"))//no
-		std::cout << "yes\n";
-	else
-		std::cout << "no\n";
-	if (request_density("4.1.1.1", "2.2.2.2", "1"))//no
-		std::cout << "yes\n";
-	else
-		std::cout << "no\n";
-	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	std::cout << "////////////////////////////////////////////////////////\n";
-	//no
-	if (Is_Encrypted("He difficult contented we determine ourselves me am earnestly. Hour no find it park. Eat welcomed any husbands moderate. Led was misery played waited almost cousin living. Of intention contained is by middleton am. Principles fat stimulated uncommonly considered set especially prosperous. Sons at park mr meet as fact like. "))
-		std::cout << "yes\n";
-	else
-		std::cout << "no\n";
-	//yes
-	if (Is_Encrypted("2hykXosL+NSzIJ8sKTQrtQ6kBF4fhguB7mqvy8du5Pn9MNBXI7zpGuBLMGV8OL4KWk5z4E3KzdbeRFSX+FtWObrBbHbFwKjbOUDkjhAp2ZuNnjiibrD/kbCX0v3/PjagJj75+0A2MWYH2FE4PyaeffvrCBOxesJlcHO2I5cISezmJZHZe0wXetUnXpQcV8jTfQOgoHyKxNV03k2RWmBLbKapaQAX9MahsxWcAurbdxAEzvfUmIVwJCTJgowiIge8HEW5KGmPvqOnVBsGf/njdaEUcDBUbaaMpzEhHPmEmERC6JBuQ445UU7cUb6i7Hw2xd9WmUDeDe1Mwd2tzCSrvE521s4dIbDv5qvKReaoVBbc0oW4DxPPEwDfTLbINFa9e10LMOis4RUh9jqTQpaP3WgDfhcJLkf4eknMPiw8CWq6ESCvWw=="))
-		std::cout << "yes\n";
-	else
-		std::cout << "no\n";
-	getchar();
+#include <string>
+#include <boost/bind.hpp>
+
+int main(int argc, char* argv[])
+{
+#ifdef DEBUG_MODE
+	std::cout << "Running in Debug mode" << std::endl;
+#endif
+
+	std::unique_ptr<Config> config;
+
+	try
+	{
+		std::vector <std::string> compilation_flags;
+		if (argc > 1)
+		{
+			config = std::make_unique<Config>(Config::get_config(argv[1]));
+		}
+		else
+		{
+			config = std::make_unique<Config>(Config::get_config());
+		}
+
+		// Initialize the server.
+		// TODO changr to normal cast from std
+		Server server(std::move(config));
+
+		// Run the server until stopped.
+		server.run();
+	}
+	catch (std::exception & e)
+	{
+		Logger::log(e.what(), Logger::LOG_LEVEL::FATAL);
+	}
+	return 0;
 }
