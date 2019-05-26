5cc77456fb49c686332be51c2aeb08db1b10a113
alon blecher <alonbl12@gmail.com>
2019-05-26 19:32:45 +0300

    added tests to proxy

--- src/checkPacket.cpp
+++ src/checkPacket.cpp
@@ -1,12 +1,12 @@
 #include "checkPacket.hpp"
 
-bool isForbidden(const std::string& srcIP, const std::string& dstIP, const std::string& data)
+bool isForbidden(const std::string& srcIP, const std::string& dstIP, const std::string& srcPort, const std::string& dstPort, const std::string& data)
 {
 	
 	//checks if the packet was sent in the same frequanty
 	//checks if the data contains unallowed words
 	//checks if the packet was sent to a malicious IP
-	if (isInterval(srcIP, dstIP) || checkIfDataConfidential(data) || checkIfMaliciousIp(dstIP))
+	if (isInterval(srcIP, dstIP) || checkIfDataConfidential(data) || checkIfMaliciousIp(dstIP) || request_density(srcIP, dstIP, data) || Is_Encrypted(data) || port_density(srcIP, dstIP, srcPort, dstPort))
 	{
 		// TO BLOCK MESSAGE
 		writeRequestToFile(srcIP, dstIP, data);
