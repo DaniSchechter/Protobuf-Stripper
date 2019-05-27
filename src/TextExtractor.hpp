85f73b0fde21e05a4733c5e350a9d6bb32c3c598
blecheral <blecheral@WS18>
2019-05-27 14:51:07 +0300

    ftp tests

---
+++ src/TextExtractor.hpp
@@ -0,0 +1,46 @@
+
+#ifndef _TEXT_EXTRACTOR_H_
+#define _TEXT_EXTRACTOR_H_
+
+#include <podofo.h>
+#include <cstdio>
+
+using namespace PoDoFo;
+
+#ifndef MAX_PATH
+#define MAX_PATH 512
+#endif // MAX_PATH
+
+/** This class uses the PoDoFo lib to parse
+ *  a PDF file and to write all text it finds
+ *  in this PDF document to stdout.
+ */
+class TextExtractor {
+public:
+	TextExtractor();
+	virtual ~TextExtractor();
+
+	void Init(const char* pszInput);
+
+private:
+	/** Extract all text from the given page
+	 *
+	 *  \param pDocument the owning document
+	 *  \param pPage extract the text of this page.
+	 */
+	void ExtractText(PdfMemDocument* pDocument, PdfPage* pPage);
+
+	/** Adds a text string to a list which can be sorted by
+	 *  position on the page later, so that the whole structure
+	 *  of the text including formatting can be reconstructed.
+	 *
+	 *  \param dCurPosX x position of the text
+	 *  \param dCurPosY y position of the text
+	 *  \param pCurFont font of the text
+	 *  \param rString the actual string
+	 */
+	void AddTextElement(double dCurPosX, double dCurPosY,
+		PdfFont* pCurFont, const PdfString & rString);
+};
+
+#endif // _TEXT_EXTRACTOR_H_
---
+++ src/ftpTests.cpp
@@ -0,0 +1,12 @@
+#include <unordered_map>
+#include <string>
+#include <set>
+#include"TestsConfig.hpp"
+#include "textExtractor.h"
+bool ftp_tests(const char data[])
+{
+// if pdf
+	TextExtractor extractor;
+	extractor.Init(data);
+}
+
