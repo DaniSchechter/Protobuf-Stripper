2b7518d0e4cc9e059637d8d0e9c731f6ba0c75b2
alex <alex.schechter@appcard.com>
2019-06-10 18:26:40 +0300

    estetic fixes

--- src/messageDecoder.hpp
+++ src/messageDecoder.hpp
@@ -1,5 +1,5 @@
-#ifndef MESSAGEDECODER_HPP_
-#define MESSAGEDECODER_HPP_
+#ifndef MESSAGE_DECODER_HPP_
+#define MESSAGE_DECODER_HPP_
 
 #include <vector>
 #include <google/protobuf/compiler/importer.h>
@@ -9,15 +9,17 @@
 #include <numeric>
 class MessageDecoder
 {
-
 public:
-MessageDecoder();
-void get_decoded_messages(std::string& data);
-bool is_proto_message(const std::string& data);
+
+    MessageDecoder();
+    void get_decoded_messages(std::string& data);
+    bool is_proto_message(const std::string& data);
 
 private:
-google::protobuf::compiler::DiskSourceTree discSourceTree_;
-google::protobuf::DynamicMessageFactory factory_;
+
+    google::protobuf::compiler::DiskSourceTree discSourceTree_;
+    google::protobuf::DynamicMessageFactory factory_;
+
 };
 
-#endif 
+#endif //MESSAGE_DECODER_HPP_
